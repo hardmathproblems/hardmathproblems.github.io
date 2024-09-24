62c14c34dca15222a5b514e06cea5080d3fb8d70
cobblesteve-dev <channelediting521@gmail.com>
2024-09-24 17:30:01 -0500

    just a sec

--- index.js
+++ index.js
@@ -26,3 +26,25 @@ document.addEventListener('keydown', function(event) {
         }
     }
 });
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2of1NPQzclRE9LGLcPQTzknyJFHRZECQ",
+  authDomain: "excellent-guard-435619-i4.firebaseapp.com",
+  projectId: "excellent-guard-435619-i4",
+  storageBucket: "excellent-guard-435619-i4.appspot.com",
+  messagingSenderId: "464990060717",
+  appId: "1:464990060717:web:60b6d20588c64f56d20624",
+  measurementId: "G-KMH6PS08ZM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
