46d61276e1098c0e971e734742973a97f437078f
SprintingSnail <dashingcuddler@gmail.com>
2024-10-09 16:57:09 -0500

    nvm i removed

--- popup.js
+++ popup.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
         closeBtn.addEventListener("click", function() {
             closePopup();
-            <audio controls autoplay src="/audio/studymusic.mp3"></audio>
         });
         
 
